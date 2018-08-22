b50498092a0216af1a015d2c487c33201322f7fd
存林 刘 <cunlin@bupt.edu.cn>
2018-08-22 23:27:18 +0800

    1.1

---
+++ opengl/源.cpp
@@ -0,0 +1,96 @@
+﻿#include<GL/glut.h>  
+#include <windows.h>
+#define WIDTH 800  
+#define HEIGHT 800  
+
+static GLfloat angle = 0.0f;
+
+void myDisplay()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	//创建透视效果视图  
+	glMatrixMode(GL_PROJECTION); //操作投影矩阵  
+	glLoadIdentity();  //当前矩阵设置为单位矩阵  
+	gluPerspective(90.0f, 1.0f, 1.0f, 20.0f);  //得到透视效果 （可视角，高宽比，最近可视距离，最远可视距离）
+	glMatrixMode(GL_MODELVIEW); //操作“模型视图”矩阵  
+	glLoadIdentity();
+	gluLookAt(0.0, 0.0, -10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+
+	//定义太阳光源，它是一种白色光源  
+	{
+		GLfloat sun_light_position[] = { 0.0f,0.0f,0.0f,1.0f };
+		GLfloat sun_light_ambient[] = { 0.0f,0.0f,0.0f,1.0f };
+		GLfloat sun_light_diffuse[] = { 0.0f,0.0f,0.0f,0.0f };
+		GLfloat sun_light_specular[] = { 1.0f,1.0f,1.0f,1.0f };
+
+		glLightfv(GL_LIGHT0, GL_POSITION, sun_light_position);
+		glLightfv(GL_LIGHT0, GL_AMBIENT, sun_light_ambient);
+		glLightfv(GL_LIGHT0, GL_DIFFUSE, sun_light_diffuse);
+		glLightfv(GL_LIGHT0, GL_SPECULAR, sun_light_specular);
+
+		glEnable(GL_LIGHT0);
+		glEnable(GL_LIGHTING);
+		glEnable(GL_DEPTH_TEST);
+	}
+
+	//定义太阳的材质并绘制太阳  
+	{
+		GLfloat sun_mat_ambient[] = { 0.0f,0.0f,0.0f,1.0f };
+		GLfloat sun_mat_diffuse[] = { 0.0f,0.0f,0.0f,1.0f };
+		GLfloat sun_mat_specular[] = { 1.0f,1.0f,1.0f,1.0f };
+		GLfloat sun_mat_emission[] = { 0.9f,0.9f,0.0f,1.0f };
+		GLfloat sun_mat_shininess = 0.0f;
+
+		glMaterialfv(GL_FRONT, GL_AMBIENT, sun_mat_ambient); //环境变量  
+		glMaterialfv(GL_FRONT, GL_DIFFUSE, sun_mat_diffuse); //散射模式  
+		glMaterialfv(GL_FRONT, GL_SPECULAR, sun_mat_specular); //镜面反射  
+		glMaterialfv(GL_FRONT, GL_EMISSION, sun_mat_emission); //发射，散发喷射  
+		glMaterialf(GL_FRONT, GL_SHININESS, sun_mat_shininess);
+
+		glutSolidSphere(2.0, 40, 32);
+	}
+
+	//定义地球材质并绘制地球  
+	{
+		GLfloat earth_mat_ambient[] = { 0.0f,0.5f,0.9f,1.0f };
+		GLfloat earth_mat_diffuse[] = { 0.0f,0.5f,0.5f,1.0f };
+		GLfloat earth_mat_specular[] = { 1.0f,1.0f,1.0f,1.0f };
+		GLfloat earth_mat_emission[] = {0.0f,0.0f,0.0f,1.0f };
+		GLfloat earth_mat_shininess = 5.0f; //镜面指数 0~128 越小越接近镜面
+
+		glMaterialfv(GL_FRONT, GL_AMBIENT, earth_mat_ambient); //环境变量  
+		glMaterialfv(GL_FRONT, GL_DIFFUSE, earth_mat_diffuse); //散射模式  
+		glMaterialfv(GL_FRONT, GL_SPECULAR, earth_mat_specular); //镜面反射  
+		glMaterialfv(GL_FRONT, GL_EMISSION, earth_mat_emission); //发射，散发喷射  
+		glMaterialf(GL_FRONT, GL_SHININESS, earth_mat_shininess);
+
+		glRotatef(angle, 0.0f, -1.0f, 0.0f);
+		glTranslatef(5.0f, 0.0f, 0.0f);
+		glutSolidSphere(1.0, 1000, 1000);//半径为 2,40 条纬线,32 条经线
+	}
+	glutSwapBuffers();
+}
+
+void myIdle()
+{
+	angle += 1.0f;
+	if (angle >= 360.0f)
+		angle = 0.0f;
+	myDisplay();
+	Sleep(20);
+}
+
+/*
+int main(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+	glutInitWindowPosition(200, 20);
+	glutInitWindowSize(WIDTH, HEIGHT);
+	glutCreateWindow("opengl光照演示");
+	glutDisplayFunc(&myDisplay);
+	glutIdleFunc(&myIdle); //回调  
+	glutMainLoop();
+	return 0;
+}*/
---
+++ opengl/源1.cpp
@@ -0,0 +1,79 @@
+#include<GL/glut.h>  
+#include<math.h>  
+#include<windows.h>  
+
+#define WIDTH 400  
+#define HEIGHT 400  
+
+#define ColoredVertex(c,v) do{glColor3fv(c);glVertex3fv(v);}while(0)  
+
+GLfloat angle = 0.0f;
+
+void myDisplay()
+{
+	static int list = 0;
+	if (list == 0)
+	{
+		GLfloat
+			PointA[] = { 0.5f,-sqrt(6.0f) / 12,-sqrt(3.0f) / 6 },
+			PointB[] = { -0.5f,-sqrt(6.0f) / 12,-sqrt(3.0f) / 6 },
+			PointC[] = { 0.0f,-sqrt(6.0f) / 12,sqrt(3.0f) / 3 },
+			PointD[] = { 0.0f,sqrt(6.0f) / 4,0 };
+		GLfloat
+			ColorR[] = { 1,0,0 },
+			ColorG[] = { 0,1,0 },
+			ColorB[] = { 0,0,1 },
+			ColorY[] = { 1,1,0 };
+
+		list = glGenLists(1); //申请1个显示列表名，并赋值给刚才创建的现实列表变量
+		glNewList(list, GL_COMPILE);
+		glBegin(GL_TRIANGLES);
+		ColoredVertex(ColorR, PointA);
+		ColoredVertex(ColorG, PointB);
+		ColoredVertex(ColorB, PointC);
+
+		ColoredVertex(ColorR, PointA);
+		ColoredVertex(ColorB, PointC);
+		ColoredVertex(ColorY, PointD);
+
+		ColoredVertex(ColorB, PointC);
+		ColoredVertex(ColorG, PointB);
+		ColoredVertex(ColorY, PointD);
+
+		ColoredVertex(ColorG, PointB);
+		ColoredVertex(ColorR, PointA);
+		ColoredVertex(ColorY, PointD);
+		glEnd();
+		glEndList();
+		glEnable(GL_DEPTH_TEST);
+	}
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glPushMatrix();
+	glRotatef(angle, 1, 0.5, 0);
+	glCallList(list);
+	glPopMatrix();
+	glutSwapBuffers();
+}
+
+void myIdle()
+{
+	++angle;
+	if (angle >= 360.0f)
+		angle = 0.0f;
+	Sleep(1000 / 10);
+	myDisplay();
+}
+
+/*
+int main(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+	glutInitWindowPosition(200, 200);
+	glutInitWindowSize(400, 400);
+	glutCreateWindow("study");
+	glutDisplayFunc(&myDisplay);
+	glutIdleFunc(&myIdle);
+	glutMainLoop();
+	return 0;
+}*/
